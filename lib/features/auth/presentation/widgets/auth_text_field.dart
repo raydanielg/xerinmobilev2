316b5603c6d24bf3d2133cdef02f783d4fb9966e
raydanielg <airezra2+1@gmail.com>
2026-06-26 22:10:22 +0300

    commit

---
+++ lib/features/auth/presentation/widgets/auth_text_field.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class AuthTextField extends StatefulWidget {
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final String label;
+  final String hint;
+  final IconData icon;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final Widget? suffix;
+  final String? Function(String?)? validator;
+  final int? maxLength;
+  final void Function(String)? onChanged;
+
+  const AuthTextField({
+    super.key,
+    this.controller,
+    this.focusNode,
+    required this.label,
+    required this.hint,
+    required this.icon,
+    this.keyboardType,
+    this.obscureText = false,
+    this.suffix,
+    this.validator,
+    this.maxLength,
+    this.onChanged,
+  });
+
+  @override
+  State<AuthTextField> createState() => _AuthTextFieldState();
+}
+
+class _AuthTextFieldState extends State<AuthTextField> {
+  late final FocusNode _internalFocusNode;
+  bool _isFocused = false;
+
+  FocusNode get _effectiveFocusNode => widget.focusNode ?? _internalFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _internalFocusNode = FocusNode();
+    _effectiveFocusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _effectiveFocusNode.removeListener(_onFocusChange);
+    _internalFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    setState(() => _isFocused = _effectiveFocusNode.hasFocus);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          widget.label,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: colorScheme.onSurface.withValues(alpha: 0.85),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: _isFocused
+                    ? colorScheme.primary.withValues(alpha: 0.15)
+                    : Colors.black.withValues(alpha: 0.03),
+                blurRadius: _isFocused ? 6 : 3,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: TextFormField(
+            controller: widget.controller,
+            focusNode: _effectiveFocusNode,
+            keyboardType: widget.keyboardType,
+            obscureText: widget.obscureText,
+            validator: widget.validator,
+            maxLength: widget.maxLength,
+            onChanged: widget.onChanged,
+            style: TextStyle(
+              fontSize: 14,
+              color: colorScheme.onSurface,
+              letterSpacing: 0.2,
+            ),
+            decoration: InputDecoration(
+              hintText: widget.hint,
+              counterText: '',
+              filled: true,
+              fillColor: colorScheme.surface.withValues(alpha: 0.6),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              prefixIcon: Icon(
+                widget.icon,
+                size: 20,
+                color: _isFocused
+                    ? colorScheme.primary
+                    : colorScheme.onSurface.withValues(alpha: 0.4),
+              ),
+              suffixIcon: widget.suffix,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  color: colorScheme.onSurface.withValues(alpha: 0.1),
+                  width: 1,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  color: colorScheme.onSurface.withValues(alpha: 0.1),
+                  width: 1,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  color: colorScheme.primary,
+                  width: 2,
+                ),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  color: colorScheme.error,
+                  width: 1,
+                ),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  color: colorScheme.error,
+                  width: 2,
+                ),
+              ),
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: colorScheme.onSurface.withValues(alpha: 0.35),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
